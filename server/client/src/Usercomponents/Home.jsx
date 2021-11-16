6443c1c7fd6aba363f88c6861cbf5dbe41047809
Expert Hacker <63892972+Expert-Hacker@users.noreply.github.com>
2021-11-16 18:28:56 +0530

    Update Home.jsx
    ssaving some codes

--- server/client/src/Usercomponents/Home.jsx
+++ server/client/src/Usercomponents/Home.jsx
@@ -18,7 +18,7 @@ function Home() {
                <div className="home_body1 d-flex">
                     <div className="d-flex flex-column m-auto">
                         <span className="display-2 mt-5 head_home">India's Cheapest SMM Service</span>
-                        <span className="text-dark caption">Get your social accounts Likes, Followers, Subscribers & More in cheap price at One Place</span>
+                        <span className="text-dark caption">111 Get your social accounts Likes, Followers, Subscribers & More in cheap price at One Place</span>
                         <Button onClick={navigate} className="w-25 btnn font-weight-bold mr-auto" variant="contained" color="warning">GET STARTED <i class="fas ml-1 fa-arrow-right"></i></Button>
                     </div>
                     <img src={img2} alt="image" height="700px" width="700px" className="mt-5 img-fluid"/>
